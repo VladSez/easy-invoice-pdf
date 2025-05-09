401a6f229f24166aeb88d8380da74abe1e373b0d
VladSez <16666234+VladSez@users.noreply.github.com>
2025-05-09 02:48:43 +0200

    fix: adjust width of loading and header components for improved layout consistency
    - Updated the width of the loading component and header section in the About page from 50% to 53% for better alignment and visual balance.
    - Enhanced the styling of the BlackGoToAppButton by adding padding for a more polished appearance.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -135,7 +135,7 @@ function Header({ locale }: { locale: Locale }) {
       <div className="flex items-center justify-center">
         <div className="container h-auto px-3 py-2 sm:h-16 sm:py-0">
           <div className="flex h-full flex-row flex-wrap items-center justify-between gap-2">
-            <div className="w-[50%]">
+            <div className="w-[53%]">
               <Logo />
             </div>
             <div className="flex items-center sm:mt-0 sm:gap-2">
@@ -143,7 +143,7 @@ function Header({ locale }: { locale: Locale }) {
                 locale={locale}
                 buttonText={t("switchLanguage")}
               />
-              <BlackGoToAppButton />
+              <BlackGoToAppButton className="px-3 sm:px-8" />
             </div>
           </div>
         </div>
