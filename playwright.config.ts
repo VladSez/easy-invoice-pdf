0fdfb972a1daffe70603f4ff1dd09937c1d8fdb3
Vlad Sazonau <16666234+VladSez@users.noreply.github.com>
2025-11-15 00:47:45 +0000

    fix: increase expect timeout for assertions to improve test reliability

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   workers: process.env.CI ? 2 : undefined,
   /* timeout for expect assertions */
   expect: {
-    timeout: 35_000,
+    timeout: 60_000,
   },
 
   // /* timeout for test execution */
