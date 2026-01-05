bb260fa93304c83d30e18a21017f4553f2f89aff
VladSez <16666234+VladSez@users.noreply.github.com>
2026-01-05 01:40:05 +0100

    chore: enhance PDF rendering utility with documentation and font face option
    * Added documentation link for PDF.js API to improve developer reference.
    * Updated PDF rendering function to disable font face for better compatibility during rendering.

--- e2e/utils/render-pdf-on-canvas.ts
+++ e2e/utils/render-pdf-on-canvas.ts
@@ -3,6 +3,8 @@ import type { Page } from "@playwright/test";
 /**
  * The most **reliable** way (cross-browser and cross-platform) to screenshot a PDF is to use a canvas and render the PDF to it
  * https://github.com/karlhorky/playwright-tricks?tab=readme-ov-file#screenshot-comparison-tests-of-pdfs
+ *
+ * **Docs**: https://mozilla.github.io/pdf.js/api/draft/module-pdfjsLib.html
  */
 export async function renderPdfOnCanvas(page: Page, pdfBytes: Uint8Array) {
   await page.setContent(`
@@ -29,7 +31,9 @@ export async function renderPdfOnCanvas(page: Page, pdfBytes: Uint8Array) {
 
                 const pdfData = new Uint8Array([${pdfBytes.join(",")}])
 
-                const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise
+                const pdf = await pdfjsLib.getDocument({ data: pdfData, disableFontFace: true }).promise;
+
+
                 const page = await pdf.getPage(1)
 
                 const viewport = page.getViewport({ scale: 1.5 })
