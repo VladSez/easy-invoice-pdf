be6637663729f9b79896591c9d0fec58d98da773
VladSez <16666234+VladSez@users.noreply.github.com>
2026-03-11 00:10:56 +0100

    feat: adjust layout in invoice form and loading components
    * Changed the lint-staged command in package.json to use pnpm for better consistency.
    * Adjusted margin styles in the invoice form component to improve layout.
    * Enhanced the loading component by refining the structure and adding placeholders for better user experience during loading states.

--- src/app/[locale]/about/loading.tsx
+++ src/app/[locale]/about/loading.tsx
@@ -1,24 +1,41 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import { ProjectLogo } from "@/components/etc/project-logo";
 
 export default function AboutLoading() {
   return (
     <div className="flex min-h-screen flex-col bg-slate-50">
       <header className="sticky top-0 z-50 w-full border-b bg-white shadow-sm">
         <div className="flex items-center justify-center">
-          <div className="container h-auto px-3 py-2 sm:h-16 sm:py-0">
+          <div className="container h-auto px-4 py-2 sm:h-16 sm:py-0 md:px-6">
             <div className="flex h-full flex-row flex-wrap items-center justify-between gap-2">
-              <div className="w-[53%]">
-                <div className="flex items-center gap-1 sm:gap-2">
-                  <ProjectLogo className="h-7 w-7 flex-shrink-0 sm:h-8 sm:w-8" />
-                  <div className="flex flex-col">
-                    <Skeleton className="h-6 w-40" />
-                    <Skeleton className="mt-1 h-3 w-24" />
+              <div className="w-[53%] sm:w-auto">
+                <div className="flex items-center gap-1.5 md:gap-2">
+                  {/* Placeholder for logo icon*/}
+                  <div className="flex items-center justify-center rounded-md">
+                    <Skeleton className="size-6 md:size-7" />
+                  </div>
+                  {/* Placeholder for app name and description on desktop */}
+                  <div className="hidden sm:block">
+                    <div className="flex flex-col space-y-1">
+                      {/* Placeholder for app name */}
+                      <Skeleton className="h-6 w-36 lg:w-40" />
+                      {/* Placeholder for app description */}
+                      <Skeleton className="mt-1.5 h-3 w-48 rounded" />
+                    </div>
+                  </div>
+                  {/* Placeholder for app name on mobile */}
+                  <div className="block sm:hidden">
+                    {/* Placeholder for app name on mobile*/}
+                    <Skeleton className="h-7 w-28" />
                   </div>
                 </div>
               </div>
-              <div className="flex items-center sm:mt-0 sm:gap-2">
-                <Skeleton className="h-9 w-[120px]" />
+              <div className="flex items-center gap-1 sm:gap-2">
+                {/* Placeholder for language switcher on desktop */}
+                <Skeleton className="hidden h-9 w-[100px] rounded-md sm:block" />
+                {/* Placeholder for language switcher on mobile */}
+                <Skeleton className="h-9 w-9 rounded-md" />
+                {/* Placeholder for go to app button */}
+                <Skeleton className="h-9 min-w-[110px] max-w-[110px] rounded-md" />
               </div>
             </div>
           </div>
@@ -116,10 +133,9 @@ export default function AboutLoading() {
           <div className="flex flex-col gap-10 md:flex-row">
             <div className="space-y-4 md:w-1/3">
               <div className="flex items-center gap-1 sm:gap-2">
-                <ProjectLogo className="h-7 w-7 flex-shrink-0 sm:h-8 sm:w-8" />
+                <Skeleton className="size-6 md:size-7" />
                 <div className="flex flex-col">
                   <Skeleton className="h-6 w-40" />
-                  <Skeleton className="mt-1 h-3 w-24" />
                 </div>
               </div>
               <Skeleton className="h-16 w-full" />
