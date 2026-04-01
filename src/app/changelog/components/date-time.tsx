21aa396be216b43f176b4bdae220881072a8a741
VladSez <16666234+VladSez@users.noreply.github.com>
2026-04-01 03:17:05 +0200

    chore: update datetime classname

--- src/app/changelog/components/date-time.tsx
+++ src/app/changelog/components/date-time.tsx
@@ -7,7 +7,7 @@ export function DateTime({
 }) {
   return (
     <time
-      className="inline-flex items-center rounded-md bg-blue-50/50 px-3 py-1.5 text-sm font-medium text-blue-800 shadow-sm ring-1 ring-blue-200 transition-colors hover:bg-blue-50 hover:text-blue-800/90 dark:border-blue-800 dark:bg-blue-950 dark:text-blue-100 dark:hover:bg-blue-900 dark:hover:text-blue-50"
+      className="inline-flex items-center rounded-md bg-blue-50/50 px-3 py-1.5 text-sm font-medium text-blue-800 shadow-sm ring-1 ring-blue-200 transition-colors hover:bg-blue-50 hover:text-blue-800/90 dark:bg-blue-950 dark:text-blue-100 dark:ring-blue-800 dark:hover:bg-blue-900 dark:hover:text-blue-50"
       dateTime={dateTime}
     >
       {children}
