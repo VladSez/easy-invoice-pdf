2fb477b670e5b94645f6fe8370ed2a54876eee6a
VladSez <16666234+VladSez@users.noreply.github.com>
2026-07-27 12:52:05 +0200

    feat: add Trending on BuildHop badge to FooterLaunchBadges component for enhanced visibility

--- src/app/(components)/footer-launch-badges.tsx
+++ src/app/(components)/footer-launch-badges.tsx
@@ -101,6 +101,60 @@ export function FooterLaunchBadges() {
         />
       </a>
 
+      {/* Badge for Trending on BuildHop */}
+      <a
+        href="https://buildhop.io/discover/easyinvoicepdf-54735f67-1b04-4e01-a1ca-6ec6e716de5f"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Trending on BuildHop: EasyInvoicePDF"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="208"
+          height="60"
+          viewBox="0 0 208 60"
+        >
+          <rect
+            x="1.5"
+            y="1.5"
+            width="205"
+            height="57"
+            rx="12"
+            fill="#FFFDF7"
+            stroke="#78A7A2"
+            stroke-width="3"
+          />
+          <g transform="translate(13 9) scale(.104)">
+            <path
+              d="M328.7 93.9a50 50 0 0 0-37-33.4c-41.4-7.2-85.9 29.7-115 61.6a223 223 0 0 0-33-72.5c-19.1-27-49.9-56.2-86.1-48.3C32.5 7 16.9 30 9.5 52.7c-22 70.4-2.5 162.7 25.4 229 8.7 20.2 19 40 32 57.8 1 1.5 3.5.7 3.4-1.2a146 146 0 0 1 1-29l-.5-1c-22-45.4-33-96.3-35.5-146.6-1.2-32 .3-65.2 12-95a61 61 0 0 1 9.8-16.9q2.5-2.7 5.2-4.5c7.4-4.6 16.2 1.4 22.2 6.3 14.1 11.8 24 28.4 33.2 44.6q6.8 12 13.2 24.6a322 322 0 0 1 32 94.6c5.8 31.6 8 64 3.5 95.8a148 148 0 0 1-5 22q-2 6.3-4.8 12.3-3.6 7.5-9 13.3a24 24 0 0 1-12 7.2q-8.5 1.8-17.6-2.3-8.3-4.3-12.6-16c-11.2-34.6 6.8-83.3 22.2-115q9-17.8 19.6-34.8a3 3 0 0 0 .3-2.6q-3.6-9.7-7.4-17.2a3 3 0 0 0-5.1-.2C76 277.7 84 331.5 83.8 331.4c1.2 16.6 5.6 35.4 18.5 44.7h-.3c7.8 5.3 16.2 9 25 10a42 42 0 0 0 28.8-7 77 77 0 0 0 26.6-36.5q5.9-15.6 9-32.2c8.3-42.6 5-88.9-2-130.4a997 997 0 0 0-6-30.4 316 316 0 0 1 34-32.8c15-12 31.5-23.2 50-28.4q32.6-8.6 37.1 24.6c2 14.9 0 30.3-3.3 45.1-8 35.5-23.6 69.3-42.9 100.3a376 376 0 0 1-54.7 68.6c-5.6 15-12.4 27.2-18.8 36.6-1.1 1.7.9 3.8 2.7 2.8l.8-.6c37-24 66.7-57.5 91.1-93.8 21.8-33 40.2-69 49.5-107.5 5.2-23 7.8-47.9-.2-70.6"
+              fill="#006C67"
+            />
+          </g>
+          <text
+            x="64"
+            y="24"
+            fill="#FFA100"
+            font-size="13"
+            font-family="Avenir Next, Inter, ui-sans-serif, system-ui, sans-serif"
+            font-weight="700"
+            letter-spacing=".08em"
+          >
+            TRENDING ON
+          </text>
+          <text
+            x="64"
+            y="43"
+            fill="#053F43"
+            font-size="22"
+            font-family="Avenir Next, Inter, ui-sans-serif, system-ui, sans-serif"
+            font-weight="800"
+            letter-spacing=".06em"
+          >
+            BuildHop
+          </text>
+        </svg>
+      </a>
+
       {/* Badge for Launched on Tiny Startups */}
       <a
         href="https://www.tinystartups.com/startup/easyinvoicepdf"
