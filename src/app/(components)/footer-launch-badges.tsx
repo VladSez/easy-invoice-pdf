249b6d3ace1dfb51aa241f8515591be0a3b3d9a7
VladSez <16666234+VladSez@users.noreply.github.com>
2026-08-01 18:10:22 +0200

    feat: add Featured on DanielLaunches badge to FooterLaunchBadges component for enhanced visibility

--- src/app/(components)/footer-launch-badges.tsx
+++ src/app/(components)/footer-launch-badges.tsx
@@ -189,6 +189,16 @@ export function FooterLaunchBadges() {
         />
       </a>
 
+      {/* Badge for Featured on DanielLaunches */}
+      <a href="https://daniellaunches.com" target="_blank">
+        <img
+          src="https://daniellaunches.com/badge-light.svg"
+          alt="Featured on DanielLaunches"
+          width="220"
+          height="48"
+        />
+      </a>
+
       {/* Badge for Launched on Tiny Startups */}
       <a
         href="https://www.tinystartups.com/startup/easyinvoicepdf"
